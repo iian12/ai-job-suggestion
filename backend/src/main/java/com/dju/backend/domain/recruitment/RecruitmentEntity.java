7bc3e49cb46734fac4fc69677177d5c841fa4e61
kotamimi <kayla.oh531@gmail.com>
2026-03-18 19:49:53 +0900

    fix: fixed RecruitmentEntity

--- backend/src/main/java/com/dju/backend/domain/recruitment/RecruitmentEntity.java
+++ backend/src/main/java/com/dju/backend/domain/recruitment/RecruitmentEntity.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecruitmentEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
